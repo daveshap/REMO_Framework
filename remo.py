513133a80c24f98d4d18e22e3e5d7221e965ff36
Alfredo Fomitchenko <alfredo.fomitchenko@mail.polimi.it>
2023-04-24 20:57:10 +0200

    Refactor with Black.

--- remo.py
+++ remo.py
@@ -4,39 +4,47 @@ import os
 
 app = FastAPI()
 root_folder = os.getcwd()
-#root_folder = 'C:/raven_private/REMO/'
+# root_folder = 'C:/raven_private/REMO/'
 max_cluster_size = 5
 # REMO = Rolling Episodic Memory Organizer
 
+
 @app.post("/add_message")
-async def add_message(message: str, speaker: str, timestamp: float):
+async def add_message(
+    message: str,
+    speaker: str,
+    timestamp: float,
+):
     # Add message to REMO
     new_message = utils.create_message(message, speaker, timestamp)
-    print('\n\nADD MESSAGE -', new_message)
+    print("\n\nADD MESSAGE -", new_message)
     utils.save_message(root_folder, new_message)
 
     return {"detail": "Message added"}
 
+
 @app.get("/search")
 async def search(query: str):
     # Search the tree for relevant nodes
-    print('\n\nSEARCH -', query)
+    print("\n\nSEARCH -", query)
     taxonomy = utils.search_tree(root_folder, query)
 
     return {"results": taxonomy}
 
+
 @app.post("/rebuild_tree")
 async def rebuild_tree():
     # Trigger full tree rebuilding event
-    print('\n\nREBUILD TREE')
+    print("\n\nREBUILD TREE")
     utils.rebuild_tree(root_folder, max_cluster_size)
 
     return {"detail": "Tree rebuilding completed"}
 
+
 @app.post("/maintain_tree")
 async def maintain_tree():
     # Trigger tree maintenance event
-    print('\n\nMAINTAIN TREE')
+    print("\n\nMAINTAIN TREE")
     utils.maintain_tree(root_folder)
 
     return {"detail": "Tree maintenance completed"}
